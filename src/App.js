6da19e44913ca1e3881bf6c0e6b918747c5bb11a
varavoi <institut93@gmail,com>
2024-10-02 20:35:16 +0300

    Создаем проект Quiz

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  //let count =0;
+ 
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
