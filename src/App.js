65c0395380da7f29ea095a6f9bb38b0ad139f4b8
varavoi <institut93@gmail,com>
2024-10-02 21:40:11 +0300

    Написал массив questions, где хранятся массив вопросов и ответов на них

--- src/App.js
+++ src/App.js
@@ -2,6 +2,49 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const questions = [
+  {
+    title: 'React - это ...?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct:0,
+  },
+  {
+    title: 'Какое из этих чисел делится на 3?',
+    variants: ['12', '15', '18'],
+    correct: 2,
+   },
+   {
+    title: 'Какой город является столицей США?',
+    variants: ['Нью-Йорк', 'Лос-Анджелес', 'Вашингтон'],
+    correct: 2,
+   },
+   {
+    title: 'Кто написал "Гамлета"?',
+    variants: ['Шекспир', 'Пушкин', 'Гете'],
+    correct: 0,
+   },
+   {
+    title: 'Какой цвет у неба?',
+    variants: ['Синий', 'Зеленый', 'Красный'],
+    correct: 0,
+   },
+   {
+    title: 'Какое из этих слов - глагол?',
+    variants: ['Стол', 'Бежать', 'Книга'],
+    correct: 1,
+   },
+   {
+    title: 'Сколько дней в неделе?',
+    variants: ['6', '7', '8'],
+    correct: 1,
+   },
+   {
+    title: 'Какое из этих слов - антоним слова "холодный"?',
+    variants: ['Теплый', 'Горячий', 'Мокрый'],
+    correct: 0,
+   }
+]
+
 function Result(){
   return(
       <div className='result'>
