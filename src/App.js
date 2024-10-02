83931126f7ac26dc195c7b1c6dac69ad6e94d625
varavoi <institut93@gmail,com>
2024-10-02 21:02:36 +0300

    Написал компоненту Game, где отображены как будут выглядеть вопросы и ответы для выбора

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+function Game(){
+  return (
+    <div>
+      <div className='progress'>
+        <div style={{width:'50%'}} className='progressFill'></div>
+      </div>
+      <h1>Что такое useState?</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это стилизация объекта</li>
+      </ul>
+    </div>
+  )
+}
 function App() {
   //let count =0;
  
   return (
     <div className="App">
-      
+      <Game/>
     </div>
   );
 }
