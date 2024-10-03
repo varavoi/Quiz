682f94eeaf23555e1d89df73aeebc5f3cfc03c11
varavoi <institut93@gmail,com>
2024-10-03 10:46:31 +0300

    Вынес все компоненты в отдельные файлы, прописал логику основного меню, написал хук для переключения состояния игры setGameState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Mainmenu from "./Components/Menu";
+import Menu from "./Components/Menu";
 import Game from "./Components/Game";
 import Result from "./Components/Result";
 import { QuizContext } from "./Helpers/Contexts.js"; 
@@ -11,20 +11,24 @@ function App() {
   //let count =0;
  const [step, setStep] = React.useState(0)
  const [correct, setCorrect] = React.useState(0)
- let question = questions[step]
- const onClickVariant =(index)=>{
-  console.log(step, index)
-  setStep(step+1)
-  if(index===question.correct){
-    setCorrect(correct+1)
-  }
- }
+ const [gameState, setGameState] = React.useState('menu')
+//  let question = questions[step]
+//  const onClickVariant =(index)=>{
+//   console.log(step, index)
+//   setStep(step+1)
+//   if(index===question.correct){
+//     setCorrect(correct+1)
+//   }
+//  }
+ 
   return (
     <div className="App">
-      {step !== questions.length?
-      <Game step ={step} question={question} onClickVariant ={onClickVariant}/>:
-       <Result correct ={correct}/> 
-      }
+      <QuizContext.Provider value ={{gameState, setGameState, step, setStep, correct, setCorrect}}>
+      <h1>Викторина</h1>
+      {gameState === "menu" && <Menu /> }
+      {gameState === "game" && <Game /> }
+      {gameState === "result" && <Result /> }
+      </QuizContext.Provider>
     </div>
   );
 }
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,19 +1,31 @@
 import "./../App.css";
 import * as React from "react";
+import { QuizContext } from "../Helpers/Contexts";
 import { questions } from "../Helpers/Questions_DB_Local"; 
 
-function Game({step, question, onClickVariant}){
+function Game(){
+  const { gameState, setGameState, step, setStep, correct, setCorrect} = React.useContext(QuizContext);
+  let question = questions[step]
+  const onClickVariant =(index)=>{
+      console.log(step, index)
+      setStep(step+1)
+      if(index===question.correct){
+        setCorrect(correct+1)
+      }
+  }
+ 
     const percentage = Math.round(step/questions.length *100);
-    return (
-      <div>
-        <div className='progress'>
-          <div style={{width:`${percentage}%`}} className='progressFill'></div>
-        </div>
-        <h1>{question.title}</h1>
-        <ul>
-          {question.variants.map((text, index)=>(<li onClick={()=>onClickVariant(index)} key={text}>{text}</li>)) }
-        </ul>
-      </div>
-    )
+    return 
+        (
+          <div>
+            <div className='progress'>
+              <div style={{width:`${percentage}%`}} className='progressFill'></div>
+            </div>
+            <h1>{question.title}</h1>
+            <ul>
+              {question.variants.map((text, index)=>(<li onClick={()=>onClickVariant(index)} key={text}>{text}</li>))}
+            </ul>
+         </div>
+        )
   }
   export default Game;
